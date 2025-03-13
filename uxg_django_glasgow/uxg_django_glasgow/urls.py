1f31e38534be489a185136458524e0a5def059f9
yiyang xiao <2901430X@student.gla.ac.uk>
2025-03-13 14:46:40 +0000

    login

--- uxg_django_glasgow/uxg_django_glasgow/urls.py
+++ uxg_django_glasgow/uxg_django_glasgow/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.register, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='uxg/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='uxg/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
