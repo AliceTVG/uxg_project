99769f2ae70c521b382fdf5853a70d720a225d25
Ryan Russell <2912222R@student.gla.ac.uk>
2025-03-18 14:22:10 +0000

    All templates created, JavaScript added & CSS started

--- uxg_django_glasgow/uxg/urls.py
+++ uxg_django_glasgow/uxg/urls.py
@@ -4,6 +4,10 @@ from uxg import views
 app_name = 'uxg'
 
 urlpatterns = [
-    path("", views.index, name="index"), 
+    path("", views.index, name="index"),
+    path("communities/", views.community_page, name="communities"),
     path("createnewpost/", views.create_post, name="create_new_post"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("profile/", views.profile, name="profile"),
 ]
